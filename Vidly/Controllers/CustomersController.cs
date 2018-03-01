45da38d4ab764abd4b2ca80f518a1b5b3c93a071
Juan Pablo Pinto <juan.pinto108@gmail.com>
2018-02-28 22:23:45 -0600

    Models, controllers and migrations created

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> Customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "John" },
+            new Customer { Id = 2, Name = "Kate" }
+        };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = Customers.Find(c => c.Id == id);
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var randomMovie = new RandomMovieViewModel
+            {
+                Movie = new Movie
+                {
+                    Name = "Shrek"
+                },
+                Customers = new List<Customer> {
+                    new Customer { Name = "George" },
+                    new Customer { Name = "Jane" }
+                }
+            };
+            return View(randomMovie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
