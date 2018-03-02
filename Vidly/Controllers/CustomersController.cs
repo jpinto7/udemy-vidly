8ea1a708fa4cf1c2d5ee8e4d034ae17e01c658cc
Juan Pablo Pinto <juan.pinto108@gmail.com>
2018-03-01 23:59:00 -0600

    Included queries from customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -9,20 +8,37 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
         public List<Customer> Customers = new List<Customer>
         {
             new Customer { Id = 1, Name = "John" },
             new Customer { Id = 2, Name = "Kate" }
         };
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View(Customers);
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = Customers.Find(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
             return View(customer);
         }
     }
